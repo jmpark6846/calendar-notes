bf27494eedce0e7d42f6c2c0035c5f43cde41086
jmpark6846 <jmpark6846@gmail.com>
2018-06-07 23:35:07 +0900

    note model 추가

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Note(models.Model):
+  author = models.ForeignKey('auth.User', related_name="notes", on_delete=models.CASCADE)
+  content = models.TextField()
+  date = models.DateTimeField(blank=True, null=True)
+  created_date = models.DateTimeField(default=timezone.now)
+  updated_date = models.DateTimeField(blank=True, null=True)
+
+  def __str__(self):
+    return self.content[:100] 
+
