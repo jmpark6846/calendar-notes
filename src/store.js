d9db44ef284beef72085d79f66b9aef780a356c3
jmpark6846 <jmpark6846@gmail.com>
2018-06-07 11:00:04 +0900

    redux store 추가

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createLogger } from 'redux-logger'
+
+import { calendarReducer } from "./calendar/reducers";
+
+const root = combineReducers({
+  calendar: calendarReducer,
+})
+
+const logger = createLogger()
+const store = createStore(root, undefined, applyMiddleware(logger, saga))
+
+export default store
