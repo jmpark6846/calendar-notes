8b39c4e48f961487406a22ab62a022e1939523e1
jmpark6846 <jmpark6846@gmail.com>
2018-06-09 22:44:39 +0900

    로그인 페이지에 로그인 실패시 에러메세지 추가

--- src/page/LoginPage.js
+++ src/page/LoginPage.js
@@ -61,6 +61,7 @@ class LoginPage extends Component {
               <input name="password" value={password} onChange={this.onChange} autoComplete="current-password" type="password" className="form-control" placeholder="비밀번호"/>
             </div>
             { error && <div className="error">{errorMsg}</div> }
+            { this.props.error && <div className="error">로그인에 실패하였습니다. 아이디와 비밀번호를 확인해주세요.</div> }
             <button className="btn btn-primary" type="submit">로그인</button>
           </form>
         </div>
