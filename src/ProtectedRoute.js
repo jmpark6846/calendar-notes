b29708d74fbb6407aaab68d7167b7948d4418a2f
jmpark6846 <jmpark6846@gmail.com>
2018-06-11 14:35:19 +0900

    isAuthenticated 값에 따라 ProtectedRoute의 렌더값 변경 설정

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -6,11 +6,19 @@ import { connect } from 'react-redux'
 const ProtectedRoute = ({ component: Component, isAuthenticated, ...rest}) => 
   <Route 
     {...rest} 
-    render={props => isAuthenticated ? (
-      <Component {...props} />
-    ):(
-      <Redirect to='/login' /> )}
-    />
+    render={ props => {
+      if (isAuthenticated === undefined){
+        return <div>loading...</div>
+      }
+      else if(isAuthenticated === false){
+        return <Redirect to='/login' />
+      }
+      else{
+        return <Component {...props} />
+      }
+    }}
+  />
+    
 
 const mapState = ({user})=>({isAuthenticated: user.isAuthenticated})
 export default connect(mapState)(ProtectedRoute)
