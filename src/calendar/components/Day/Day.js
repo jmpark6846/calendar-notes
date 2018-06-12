421e16db691f392c1f0bd1deb65471840edc5885
jmpark6846 <jmpark6846@gmail.com>
2018-06-12 09:26:38 +0900

    day 컴포넌트에 proptype 추가

--- src/calendar/components/Day/Day.js
+++ src/calendar/components/Day/Day.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Day = ({date, hasNote, selectedDate, today, handleClick}) => 
   <div className={"day" + (+selectedDate === +date ? " selected" : "") + (+today === +date ? " today" : "") + (hasNote ? " has-note" : "")} onClick={handleClick(date)}>
     {date && date.getDate()}
   </div>
+
+Day.propTypes = {
+  date: PropTypes.object,
+  hasNote: PropTypes.bool,
+  selectedDate: PropTypes.object,
+  today: PropTypes.object,
+  handleClick: PropTypes.func,
+}
 
 export default Day
