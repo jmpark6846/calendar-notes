4912c307c8703b3c1ef21681ae94f8c1730ab6f2
jmpark6846 <jmpark6846@gmail.com>
2018-06-14 09:57:07 +0900

    debug 설정 끄기

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v(ay(8cc+5xw@ixq=fmckg)x)bl!6kyby-fetqn!w4pv+a!)#d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['calendar-note.herokuapp.com']
 
