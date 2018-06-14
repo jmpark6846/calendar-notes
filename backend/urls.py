8da5f817f50df50afdfafa43e17e745a41a061b5
jmpark6846 <jmpark6846@gmail.com>
2018-06-14 17:31:41 +0900

    urls 수정

--- backend/urls.py
+++ backend/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from api import views
 
 urlpatterns = [
     path('', views.FrontendAppView.as_view() ),
-    path('(?:.*)/?', views.FrontendAppView.as_view() ),
+    re_path('(?:.*)/?', views.FrontendAppView.as_view() ),
     
     path('admin/', admin.site.urls),
     path('rest-api/', include('rest_framework.urls')),
