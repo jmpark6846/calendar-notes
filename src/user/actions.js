789be68e6552a01ce8712033c695b6cbb57b1a22
jmpark6846 <jmpark6846@gmail.com>
2018-06-20 11:50:05 +0900

    user action nesting 줄이기

--- src/user/actions.js
+++ src/user/actions.js
@@ -2,55 +2,42 @@ import { USER_LOGIN, USER_LOGOUT, LOGIN_SUCCESS, LOGIN_FAIL, REGISTER_FAIL, REGI
 
 export const doUserLogin = (username, password, history) => ({
   type: USER_LOGIN,
-  payload:{
-    username,
-    password,
-    history
-  }
+  username,
+  password,
+  history
+
 })
 
 export const doLoginSuccess = (username) => ({
   type: LOGIN_SUCCESS,
-  payload: {
-    username
-  }
+  username
 })
 
 export const doLoginFail = (error) => ({
   type: LOGIN_FAIL,
-  payload: {
-    error
-  }
+  error
 })
 
 export const doUserRegister = (username, password, history) => ({
   type: USER_REGISTER,
-  payload:{
-    username,
-    password,
-    history
-  }
+  username,
+  password,
+  history
 })
 
 export const doRegisterSuccess = (username) => ({
   type: REGISTER_SUCCESS,
-  payload: {
-    username
-  }
+  username
 })
 
 export const doRegisterFail = (error) => ({
   type: REGISTER_FAIL,
-  payload: {
-    error
-  }
+  error
 })
 
 export const doUserLogout = (history) => ({
   type: USER_LOGOUT,
-  payload:{
-    history
-  }
+  history
 })
 
 export const doLogoutSuccess = () => ({
