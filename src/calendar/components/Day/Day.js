e82aa741ff066e6680acea9ad90e8c43df2916bb
jmpark6846 <jmpark6846@gmail.com>
2018-06-20 22:41:11 +0900

    day에서 classnames로 클래스 이름 표기 깔끔하게 정리

--- src/calendar/components/Day/Day.js
+++ src/calendar/components/Day/Day.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
-const Day = ({date, hasNote, selectedDate, today, handleClick}) => 
-  <div className={"day" + (+selectedDate === +date ? " selected" : "") + (+today === +date ? " today" : "") + (hasNote ? " has-note" : "") + (date ? " date" : "")} onClick={handleClick(date)}>
-    {date && date.getDate()}
-  </div>
+const Day = ({date, hasNote, selectedDate, today, handleClick}) => {
+  const classes = classNames('day', {
+    'has-note': hasNote,
+    'date': date,
+    'today': +today === +date,
+    'selected': +selectedDate === +date,
+  })
+  return (
+    <div className={classes} onClick={handleClick(date)}>
+      {date && date.getDate()}
+    </div>
+  )
+}
+  
 
 Day.propTypes = {
   date: PropTypes.object,
