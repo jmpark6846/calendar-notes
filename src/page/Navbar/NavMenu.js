97ffb1baa95960a6d4880f9e476860beac531b51
jmpark6846 <jmpark6846@gmail.com>
2018-06-21 17:44:23 +0900

    navMenu 추가

---
+++ src/page/Navbar/NavMenu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const NavMenu = ({id, justify, children}) => {
+  const j = justify === 'right' ? 'ml-auto' : 'mr-auto'
+
+  return (
+    <div className="collapse navbar-collapse" id={id}>     
+      <ul className={"navbar-nav " + j}>
+        {children}
+      </ul>
+    </div>
+  )
+}
+
+NavMenu.propTypes = {
+
+}
+
+export default NavMenu 
--- src/page/Navbar/Navbar.js
+++ src/page/Navbar/Navbar.js
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 import { doUserLogout } from '../../user/actions';
 import './Navbar.css'
 import NavItem from './NavItem';
+import NavMenu from './NavMenu';
 
 const activePage = {}
 
@@ -15,25 +16,23 @@ const Navbar = ({username, isAuthenticated, logout, history}) =>
       <NavLink to="/" className="navbar-brand">
         CalendarNote
       </NavLink>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-menu" aria-controls="navbar-collapse-menu" aria-expanded="false" aria-label="Toggle navigation">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-menu" aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       
-      <div className="collapse navbar-collapse" id="navbar-collapse-menu">     
-        { isAuthenticated ? 
-        <ul className="navbar-nav ml-auto">
-          <NavItem value={username} route='calendar' />
-          <NavItem>
-            <span className="nav-link logout" onClick={()=>logout(history)} >Logout</span>
-          </NavItem>
-        </ul>
-        :
-        <ul className="navbar-nav ml-auto">
-          <NavItem value='register' />
-          <NavItem value='login' />
-        </ul>
-        }
-      </div>
+      { isAuthenticated ? 
+      <NavMenu id='navbar-menu' justify='right' >
+        <NavItem value={username} route='calendar' />
+        <NavItem>
+          <span className="nav-link logout" onClick={()=>logout(history)}>logout</span>
+        </NavItem>
+      </NavMenu>
+      :
+      <NavMenu id='navbar-menu' justify='right' >
+        <NavItem value='register' />
+        <NavItem value='login' />
+      </NavMenu>
+      }
     </nav>  
   </header>
 
